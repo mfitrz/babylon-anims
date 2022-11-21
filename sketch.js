0aab0663b372c3db3e17354fa1067d50b1632e9c
Michal Fitrzyk <fitrzykmichal1234@gmail.com>
2022-11-20 18:54:51 -0600

    Shark attack!

--- sketch.js
+++ sketch.js
@@ -31,11 +31,40 @@ var createScene = function () {
     // Default intensity is 1. Let's dim the light a small amount
     light.intensity = 0.7;
 
-    var newsphere = createSphere(0, 1, 0, 2);
+    /*
+    var newsphere = createSphere(-2, 1, -2, 2);
     newsphere.material = hexMat('#ff0000');
 
+
+    var newbox = createBox(2, 1, 2, 2, 2, 2);
+    newbox.material = hexMat('#00a86b');
+    */
+
+    var shark = new meshModel('shark.glb', 0.5, x=0, y=0 ,z=10);
+
+    var fish = new meshModel('fish.glb', 0.5);
+
+
+    /*
+    var anim1 = {subj: newsphere.position, prop: 'y', val: 5};
+    var anim2 = {subj: newsphere.material, prop: 'alpha', val: 0};
+
+    var anim3 = {subj: newbox.rotation, prop: 'z', val: Math.PI/2};
+    var anim4 = {subj: newbox.material, prop: 'alpha', val: 0};
+    */
+
+    var anim5 = {subj: shark.position, prop: 'z', val: -10};
+    var anim6 = {subj: shark.position, prop: 'y', val: Math.PI*1.4}
+
+    var anim7 = {subj: fish.position, prop: 'y', val: -3};
+
+    var anims = [anim5, anim6, anim7];
+
+    animate(anims, scene, 2, true);
+
+
     // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
+    //var ground = BABYLON.MeshBuilder.CreateGround("ground", {width: 6, height: 6}, scene);
 
     return scene;
 };
